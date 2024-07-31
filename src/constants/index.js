3ba027952c2b84fa08492bdedb62bf7e4ef0e78d
Ozair-Hassan <ozair.hassan123@gmail.com>
2024-08-01 01:44:26 +0500

    chore: removed contact from navbar

--- src/constants/index.js
+++ src/constants/index.js
@@ -32,10 +32,10 @@ export const navLinks = [
     id: 'clients',
     title: 'Clients',
   },
-  {
-    id: 'contact',
-    title: 'Contact Us',
-  },
+  // {
+  //   id: 'contact',
+  //   title: 'Contact Us',
+  // },
 ]
 
 export const features = [
